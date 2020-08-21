de545cec6bc399924f404a79fba8535c2205d0f4
Jamestr <james.t.ringuet@gmail.com>
2020-08-21 16:31:46 +0700

    single product, start forms

--- wp-content/themes/christmastreefarm/functions.php
+++ wp-content/themes/christmastreefarm/functions.php
@@ -62,20 +62,29 @@ add_action( 'get_header', 'xm_remove_storefront_sidebar' );
 /**
  * Re-arrange breadcrumb
  */
-function xm_rearrange() {
+function xm_rearrange_breadcrumb() {
 	remove_action( 'storefront_before_content', 'woocommerce_breadcrumb', 10 );
 	add_action( 'woocommerce_before_shop_loop', 'woocommerce_breadcrumb', 5 );
 	add_action( 'storefront_page', 'woocommerce_breadcrumb', 15 );
 	add_action( 'woocommerce_before_single_product', 'woocommerce_breadcrumb', 5 );
 }
-add_action( 'init', 'xm_rearrange', 10 );
+add_action( 'init', 'xm_rearrange_breadcrumb', 10 );
 
-add_action( 'wp_print_styles', 'dequeue_xm_theme_styles', 100 );
+add_action( 'wp_print_styles', 'xm_blog_breadcrumb', 100 );
 /**
  * Add breadcrumb to blog page.
  */
-function dequeue_xm_theme_styles() {
+function xm_blog_breadcrumb() {
 	if ( is_home() ) {
 		add_action( 'storefront_content_top', 'woocommerce_breadcrumb', 10 );
 	}
 }
+
+/**
+ * Re-arrange single product
+ */
+function xm_rearrange_single() {
+	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+	add_action( 'woocommerce_product_thumbnails', 'woocommerce_template_single_meta', 25 );
+}
+add_action( 'init', 'xm_rearrange_single', 10 );
