f2ed9294a51baeeaaab04f55bed26ebae2486407
Jamestr <james.t.ringuet@gmail.com>
2020-09-29 17:16:46 +0700

    js shipping patch

--- wp-content/themes/christmastreefarm/assets/js/main.js
+++ wp-content/themes/christmastreefarm/assets/js/main.js
@@ -24,23 +24,24 @@
 
 		// Check shipping method based on custom product addon
 		function shippingCheck() {
-			if ($(".woocommerce-checkout-review-order-table:contains('home')").length > 0 &&  $(".woocommerce-checkout-review-order-table:contains('farm')").length > 0) {
+			if ($(".woocommerce-checkout-review-order-table tbody:contains('home')").length > 0 &&  $(".woocommerce-checkout-review-order-table tbody:contains('farm')").length > 0) {
 				console.log('error');
 				$('body').addClass('shippingError');
 				$('form.woocommerce-checkout').hide();
 				$('.entry-content > .woocommerce').append('<p class="woocommerce-error">Please choose your trees with either delivery or pickup | <a href="/cart/">Remove items from cart</a></p>');
 			} else {
 			if ($(".variation-PleaseselecthowyoudliketoreceiveyourChristmastree div span:contains('Delivery')").length > 0) {
-				if ($('#shipping_method_0_flat_rate28').prop('checked', true) ) {
+				console.log('contains delivery');
+				if ($('input[id^="shipping_method_0_local_pickup"').prop('checked', true) ) {
 					console.log('switch to delivery');
-					$('#shipping_method_0_flat_rate13').prop('checked', true);
+					$('input[id^="shipping_method_0_flat_rate"').prop('checked', true);
 				}
 				$('body').addClass('collectHidden');
 			} else if ($(".variation-PleaseselecthowyoudliketoreceiveyourChristmastree div span:contains('click')").length > 0) {
 				console.log('contains collect');
-				if ($('#shipping_method_0_flat_rate13').prop('checked', true) ) {
+				if ($('input[id^="shipping_method_0_flat_rate"').prop('checked', true) ) {
 					console.log('switch to collect');
-					$('#shipping_method_0_flat_rate28').prop('checked', true);
+					$('input[id^="shipping_method_0_local_pickup"').prop('checked', true);
 				}
 				$('body').addClass('deliveryHidden');
 			}
